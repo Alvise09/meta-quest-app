3c73a78aa40cc06c52b46d48da32f50ba349fba7
Alvise09 <Alvi.vivian09@gmail.com>
2024-12-01 11:56:23 +0100

    Prima versione della mia app

---
+++ script.js
@@ -0,0 +1,33 @@
+import * as THREE from 'three';
+import { VRButton } from 'three/examples/jsm/webxr/VRButton.js';
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.xr.enabled = true;
+document.body.appendChild(VRButton.createButton(renderer));
+document.body.appendChild(renderer.domElement);
+
+const geometry = new THREE.BoxGeometry();
+const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+const cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
+
+camera.position.z = 5;
+
+function animate() {
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
+    renderer.render(scene, camera);
+}
+
+renderer.setAnimationLoop(animate);
+
+
+if ("serviceWorker" in navigator) {
+    navigator.serviceWorker
+        .register("./service-worker.js")
+        .then(() => console.log("Service Worker registrato con successo"))
+        .catch((error) => console.error("Errore nel registrare il Service Worker:", error));
+}
